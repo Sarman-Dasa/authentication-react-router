d4bc7f72d5a12508af8d195856febc18940e2c53
Sarman-Dasa <sarmand@zignuts.com>
2024-06-03 14:56:38 +0530

    Check user in login or not

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 export default function Dashboard() {
-  const use  = useSelector((state)=>state.app.user);
+  const user  = useSelector((state)=>state.app.user);
+  const isloggedin = useSelector((state) => state.app.isloggedin);
+  const navigate = useNavigate()
+  useEffect(() => {
+      if(!isloggedin) {
+        navigate('/sign-in');
+      }
+  },[])
   return (
     <div>
-      <h1>Welcome ,{use.name} </h1>
+      <h1>Welcome ,{user && user.name} </h1>
     </div>
   )
 }
